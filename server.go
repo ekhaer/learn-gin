8a495fd74f2e2f71314cbb991237461128bb040e
ekhaer <emiliakhaer@gmail.com>
2022-08-10 11:07:57 +0700

    init

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+    r := gin.Default()
+    r.SetTrustedProxies([]string{"192.168.1.2"})
+
+	r.GET("/get", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "status": "healthy",
+        })
+    })
+
+    r.Run()
+
+}
